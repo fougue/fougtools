0b075e31acc010c5b7337960c91ea9df4234c06a
Hugues Delorme <delorme.hugues@fougsys.fr>
2015-03-02 15:49:15 +0100

    qttools_core: add facilities for QObject enum types (see QObjectUtils)

--- src/qttools/core/qobject_utils.h
+++ src/qttools/core/qobject_utils.h
@@ -37,8 +37,10 @@
 
 #pragma once
 
-#include "core.h"
 class QObject;
+
+#include "core.h"
+#include <vector>
 
 namespace qtcore {
 
@@ -52,6 +54,12 @@ public:
 
     template<typename PARENT_TYPE>
     static PARENT_TYPE* findParent(QObject* object);
+
+    template<typename QOBJECT, typename ENUM>
+    static ENUM toQEnumValue(const char* enumName, int value);
+
+    template<typename QOBJECT, typename ENUM>
+    static std::vector<ENUM> allQEnumValues(const char* enumName);
 };
 
 } // namespace qtcore
@@ -61,6 +69,9 @@ public:
 // --
 
 #include <QtCore/QObject>
+#include <QtCore/QMetaObject>
+#include <QtCore/QMetaEnum>
+#include <cassert>
 
 namespace qtcore {
 
@@ -77,6 +88,44 @@ PARENT_TYPE* QObjectUtils::findParent(QObject* object)
     while (it != NULL && qobject_cast<PARENT_TYPE*>(it) == NULL)
         it = it->parent();
     return qobject_cast<PARENT_TYPE*>(it);
+}
+
+//! Safe cast of an integer to a Q_ENUM value
+template<typename QOBJECT, typename ENUM>
+ENUM QObjectUtils::toQEnumValue(const char* enumName, int value)
+{
+    const QMetaObject& metaObj = QOBJECT::staticMetaObject;
+    const int enumIndex = metaObj.indexOfEnumerator(enumName);
+    if (enumIndex != -1) {
+        const QMetaEnum metaEnum = metaObj.enumerator(enumIndex);
+        if (metaEnum.valueToKey(value) != nullptr)
+            return static_cast<ENUM>(value);
+    }
+    assert(false);
+    return static_cast<ENUM>(value);
+}
+
+/*! \brief All enumerator values of QOBJECT::ENUM returned in a single array
+ *
+ *  \tparam QOBJECT A QObject type, or a class marked at least with Q_GADGET (eg. QLocale)
+ *  \tparam ENUM An enum type, declared in QOBJECT and marked with Q_ENUM (eg. QLocale::Country)
+ *  \param enumName Name of the enumerator type (eg. "Country" for QLocale::Country)
+ */
+template<typename QOBJECT, typename ENUM>
+std::vector<ENUM> QObjectUtils::allQEnumValues(const char* enumName)
+{
+    std::vector<ENUM> enumValueVec;
+    const int enumIndex = QOBJECT::staticMetaObject.indexOfEnumerator(enumName);
+    if (enumIndex != -1) {
+        const QMetaEnum metaEnum = QOBJECT::staticMetaObject.enumerator(enumIndex);
+        enumValueVec.reserve(metaEnum.keyCount());
+        for (int i = 0; i < metaEnum.keyCount(); ++i) {
+            const int enumValueAsInt = metaEnum.value(i);
+            if (enumValueAsInt != -1)
+                enumValueVec.push_back(static_cast<ENUM>(enumValueAsInt));
+        }
+    }
+    return enumValueVec;
 }
 
 } // namespace qtcore

3be2a4b46af7a4c0ff41f827aca7e6583b096a7b
Hugues Delorme <delorme.hugues@fougsys.fr>
2015-07-02 17:33:25 +0200

    qttools_core: add QStringUtils::iequals() functions

--- src/qttools/core/qstring_utils.h
+++ src/qttools/core/qstring_utils.h
@@ -42,6 +42,27 @@
 
 namespace qtcore {
 
+/*! \class QStringUtils
+ *  \brief Provides a collection of tools around QString
+ *  \headerfile qstring_utils.h <qttools/core/qstring_utils.h>
+ *  \ingroup qttools_core
+ */
+class QTTOOLS_CORE_EXPORT QStringUtils
+{
+public:
+    /*! Shorter equivalent to QString::compare(s1, s2, Qt::CaseInsensitive) */
+    inline static bool iequals(const QString& s1, const QString& s2);
+
+    //! \overload
+    inline static bool iequals(const QString& s1, QLatin1String s2);
+    //! \overload
+    inline static bool iequals(QLatin1String s1, const QString& s2);
+    //! \overload
+    inline static bool iequals(const QString& s1, const QStringRef& s2);
+    //! \overload
+    inline static bool iequals(const QStringRef& s1, const QString& s2);
+};
+
 /*! Functor wrapper around QString::localeAwareCompare(lhs, rhs) == 0
  *  \headerfile qstring_utils.h <qttools/core/qstring_utils.h>
  *  \ingroup qttools_core
@@ -101,5 +122,36 @@ struct QTTOOLS_CORE_EXPORT QStringLocaleAwareGreaterEqual
     inline bool operator()(const QString& lhs, const QString& rhs) const
     { return QString::localeAwareCompare(lhs, rhs) >= 0; }
 };
+
+
+// --
+// -- Implementation
+// --
+
+bool QStringUtils::iequals(const QString &s1, const QString &s2)
+{
+    return QString::compare(s1, s2, Qt::CaseInsensitive) == 0;
+}
+
+bool QStringUtils::iequals(const QString &s1, QLatin1String s2)
+{
+    return QString::compare(s1, s2, Qt::CaseInsensitive) == 0;
+}
+
+bool QStringUtils::iequals(QLatin1String s1, const QString &s2)
+{
+    return QString::compare(s1, s2, Qt::CaseInsensitive) == 0;
+}
+
+bool QStringUtils::iequals(const QString& s1, const QStringRef& s2)
+{
+    return QString::compare(s1, s2, Qt::CaseInsensitive) == 0;
+}
+
+bool QStringUtils::iequals(const QStringRef& s1, const QString& s2)
+{
+    // There is no QString::compare(QStringRef, QString) overload
+    return QString::compare(s2, s1, Qt::CaseInsensitive) == 0;
+}
 
 } // namespace qtcore
